e9aea8a8fabb27e5c8977f3788fdf8c9794b0dea
Ana Flávia Ferreira <ferreiraanaflaviaalves@gmail.com>
2022-09-13 19:31:31 -0300

    Ajustes candidato

--- src/main/java/com/workshop/voting_intention/model/repository/CandidateRepository.java
+++ src/main/java/com/workshop/voting_intention/model/repository/CandidateRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
-    // public static final String FIND_PROJECTS = "select candidato.*, count(usuario.id_candidato) as quantidade, (select count(usuario.id) from voto.usuario) as totalVotos from voto.candidato left join voto.usuario ON usuario.id_candidato = candidato.id GROUP by candidato.id";
-    // public static final String FIND_PROJECTS = "select * from candidato";
 
     @Query(value = "select candidato.*, count(usuario.voto) as quantidade, (select count(usuario.id) from voto.usuario) as total_votos from voto.candidato left join voto.usuario ON usuario.voto = candidato.id GROUP by candidato.id", nativeQuery = true)
     List<Candidate> findAll();
